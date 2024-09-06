d3a7d3098ec198ded27b519ad6fe362ca13f9968
SohamShitole <shitolesoham@gmail.com>
2024-09-05 23:52:06 -0700

    Third code

--- Leetcode/Two_sum.py
+++ Leetcode/Two_sum.py
@@ -1,8 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                a = nums[i] + nums[j]
-                if a == target:
-                    b = [i,j]
-                    return b
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         a = nums[i] + nums[j]
+        #         if a == target:
+        #             b = [i,j]
+        #             return b
+
+        num_dict = {}
+        for index,num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], index]
+            num_dict[num] = index
